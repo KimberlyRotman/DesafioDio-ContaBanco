d304076b2b50ca0ee7b2fed425e951a75ba1d9c1
KimberlyRotman <kimberlyrotmanms@gmail.com>
2024-05-29 17:00:59 -0300

    Adição de nova interação com o usuário - deposito/saque

--- ContaBanco/src/ContaBanco.java
+++ ContaBanco/src/ContaBanco.java
@@ -17,6 +17,28 @@ public class ContaBanco {
             double saldo = scanner.nextDouble();
 
             System.out.println("Ola " + nome + ", obrigado por criar uma conta em nosso banco. Sua agência é " + agencia + ",conta " + conta + ", e seu saldo " + saldo + " já está disponível para saque.");
+            
+            System.out.println("Para Depositar digite '1'. Para Sacar digite '2'. Para sair digite '3'");
+            int acao = scanner.nextInt();
+
+            if(acao == 1){
+                System.out.println("Digite o valor do depósito: ");
+                double deposito = scanner.nextDouble();
+                saldo = saldo + deposito;
+                System.out.println("seu saldo atual é " + saldo);
+            }
+            if(acao == 2){
+                System.out.println("Digite valor do saque: ");
+                double saque = scanner.nextDouble();
+                if (saque <= saldo){
+                    System.out.println("Saque efetuado.");
+                    saldo = saldo - saque;
+                }
+                if (saque > saldo){
+                    System.out.println("Saque recusado");
+                }
+                System.out.println("Seu saldo atual é " + saldo);
+            }
         }
     }
 }
